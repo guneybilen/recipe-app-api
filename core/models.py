c7733a7b3716149cdaa1fa9c656e763a146eb6fc
guney bilen <guneybilen@yahoo.com>
2021-12-05 00:41:33 -0500

    Added Recipe model.

--- core/models.py
+++ core/models.py
@@ -56,3 +56,20 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    """Recipe object"""
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    # passing Ingredient as a string below makes the Ingredient class
+    # free of order relative to Recipe class. You can also pass as a constant
+    # bu the  Ingredient has to come before the Recipe class in the order.
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.title
